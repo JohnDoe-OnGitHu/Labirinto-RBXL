44e7c03b2018acd82ade773370d1202aa37c71d8
JohnDoe-OnGitHu <queromaisn1@gmail.com>
2026-03-11 20:29:47 -0300

    Refeição do labirinto
    Eu refiz as partes do labirinto e em-breve ou fazer um labirinto todo, que vai ser muito legal.

--- criarLabirinto.lua
+++ criarLabirinto.lua
@@ -1,4 +1,14 @@
-local workspace = game:GetService("Workspace")
+local workspace = game.Workspace
+
+local pastaLabirinto = workspace:FindFirstChild("Labirinto")
+
+if not pastaLabirinto then
+	pastaLabirinto = Instance.new("Folder")
+	pastaLabirinto.Name = "Labirinto"
+	pastaLabirinto.Parent = workspace
+end
+
+
 
 --------------------------------------------------------
 
@@ -16,9 +26,14 @@ local yChao 		= 0
 local spawn = workspace:WaitForChild("SpawnLocation")
 spawn.Position = Vector3.new(-43.5, yChao + 1, 43)
 
+-- |-- CONATDORES --| --
+
+local ContadorD = 1
+local ContadorE = 1
+
 -- |-- FUNÇÕES DE CRIAR PAREDES --| --
 
-local function criarParte(tamanho, posicao, cor)
+local function criarParte(tamanho, posicao, cor, nome)
 	local part = Instance.new("Part")
 	part.Size = tamanho
 	part.Position = posicao
@@ -26,9 +41,24 @@ local function criarParte(tamanho, posicao, cor)
 	part.BrickColor = BrickColor.new(cor or "Medium stone grey")
 	part.TopSurface = Enum.SurfaceType.Smooth
 	part.BottomSurface = Enum.SurfaceType.Smooth
-	part.Parent = workspace
+	part.Parent = pastaLabirinto
+
+	if nome then
+		part.Name = nome
+	else
+		if posicao.Z >= 0 then
+			part.Name = "D-" .. string.format("%02d", ContadorD)
+			ContadorD += 1
+		else
+			part.Name = "E-" .. string.format("%02d", ContadorE)
+			ContadorE += 1
+		end
+	end
 	return part
 end
+
+
+
 
 --------------------------------------------------------
 
@@ -39,7 +69,8 @@ end
 criarParte(
 	Vector3.new(largura, 1, comprimento),
 	Vector3.new(0, yChao, 0),
-	"Dark stone grey"
+	"Dark stone grey",
+	"Floor"
 )
 
 --------------------------------------------------------
@@ -51,7 +82,8 @@ criarParte(
 --[[criarParte(
 	Vector3.new(largura, 1, comprimento),
 	Vector3.new(0, yChao + altura, 0),
-	"Dark stone grey"
+	"Dark stone grey",
+	"Roof"
 )]]
 
 --------------------------------------------------------
@@ -62,22 +94,30 @@ criarParte(
 
 criarParte(
 	Vector3.new(largura, altura, 1),
-	Vector3.new(0, yChao + altura/2, -comprimento/2)
+	Vector3.new(0, yChao + altura/2, -comprimento/2),
+	"Medium stone grey",
+	"Wall_Front"
 )
 
 criarParte(
 	Vector3.new(largura, altura, 1),
-	Vector3.new(0, yChao + altura/2, comprimento/2)
+	Vector3.new(0, yChao + altura/2, comprimento/2),
+	"Medium stone grey",
+	"Wall_Back"
 )
 
 criarParte(
 	Vector3.new(1, altura, comprimento),
-	Vector3.new(-largura/2, yChao + altura/2, 0)
+	Vector3.new(-largura/2, yChao + altura/2, 0),
+	"Medium stone grey",
+	"Wall_Left"
 )
 
 criarParte(
 	Vector3.new(1, altura, comprimento),
-	Vector3.new(largura/2, yChao + altura/2, 0)
+	Vector3.new(largura/2, yChao + altura/2, 0),
+	"Medium stone grey",
+	"Wall_Right"
 )
 
 --------------------------------------------------------
@@ -87,6 +127,16 @@ criarParte(
 --------------------------------------------------------
 
 criarParte(
-	Vector3.new(1, altura, 60),
-	Vector3.new(-37, yChao + altura/2, 20)
+	Vector3.new(1, altura, 44),
+	Vector3.new(-37, yChao + altura/2, 28)
+)
+
+criarParte(
+	Vector3.new(18, altura, 1),
+	Vector3.new(-28.5, yChao + altura/2, 6.5)
+)
+
+criarParte(
+	Vector3.new(1, altura, 26),
+	Vector3.new(-20, yChao + altura/2, 19)
 )
